6bed6d19903693a9f1e8b116195d8af1592d02f2
Orel Zabriko <orelzavriko@gmail.com>
2026-06-03 09:40:30 +0300

    erase spaces

--- src/BuildInCommands.cpp
+++ src/BuildInCommands.cpp
@@ -10,25 +10,28 @@
 
 class ShellModel;
 // --- Command Pattern: Interface ---
-class BuiltInCommand {
+class BuiltInCommand 
+{
 private:
-    ShellModel *model;
+    ShellModel *model; //change to smart pointer
 public:
     virtual ~BuiltInCommand() = default;
-    virtual std::string getName() const = 0;
+    virtual std::string getName() const = 0; // identify the command - "cd" "myhistory"
     virtual void execute(const std::vector<std::string>& args) = 0;
     
-    public BuiltInCommand() {}
+    public BuiltInCommand() {} // not needed and illegal
     public BuiltInCommand(ShellModel *theModel) { model = theModel;}
-    
 };
 
 // --- Concrete Built-in: cd ---
-class CdCommand : public BuiltInCommand {
+class CdCommand : public BuiltInCommand 
+{
 public:
     std::string getName() const override { return "cd"; }
-    void execute(const std::vector<std::string>& args) override {
-        if (args.size() < 2) {
+    void execute(const std::vector<std::string>& args) override 
+    {
+        if (args.size() < 2) 
+        {
             std::cerr << "cd: missing argument\n";
             return;
         }
@@ -39,12 +42,13 @@ public:
 };
 
 // --- Concrete Built-in: exit ---
-class ExitCommand : public BuiltInCommand {
+class ExitCommand : public BuiltInCommand 
+{
 public:
     std::string getName() const override { return "exit"; }
-    void execute(const std::vector<std::string>& args) override {
+    void execute(const std::vector<std::string>& args) override 
+    {
         std::cout << "Exiting shell...\n";
         exit(0);
     }
 };
-
